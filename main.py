cf5671879144938b8ce4daed70751b980833c78c
yusufokkabas@windowslive.com <yusufokkabas@windowslive.com>
2023-05-12 22:07:33 +0300

    basic project structure

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+from src.scripts.concatFiles import concatFiles
+df = pd.read_csv('src/data/cleaned_data.csv')
+
+# Display the first few rows
+print(df.head())
+
+# Get the dimensions of the DataFrame
+print(df.shape)
+
+# Get a summary of the DataFrame
+print(df.info())
+
+# Generate descriptive statistics
+print(df.describe())
+
+# Get the column names
+print(df.columns)
+
+# Get the data types of the columns
+print(df.dtypes)
+
---
+++ src/scripts/concatFiles.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def concatFiles():
+# Read the CSV files, specifying the correct delimiter for both files
+    player_data = pd.read_csv('src/data/player_data.csv', sep=';')
+    market_value = pd.read_csv('src/data/players.csv', sep=';')
+
+    # Merge the dataframes using the common property 'player' using a left join
+    merged_data = player_data.merge(market_value, on='player', how='left')
+
+    # Drop rows with null values in the 'Market value' column
+    merged_data.dropna(subset=['market_value_in_eur'], inplace=True)
+
+    # Write the cleaned data to a new CSV file
+    merged_data.to_csv('src/data/cleaned_data.csv', index=False)
