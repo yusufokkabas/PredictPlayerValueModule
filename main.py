4a85944447ebc367b80c9cfdcee6faedb289e0a2
yusufokkabas@windowslive.com <yusufokkabas@windowslive.com>
2024-01-17 22:14:08 +0300

    final commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 import pandas as pd
 import numpy as np
 import pytz
-from src.scripts.dbConn import create_conn
+# from src.scripts.dbConn import create_conn
 from src.scripts.concatFiles import concatFiles
 from sklearn.base import BaseEstimator, TransformerMixin
 from src.scripts.filter_csv import filterOutliers
@@ -54,8 +54,8 @@ from sklearn.ensemble import GradientBoostingRegressor
 #         return 1.4
 
 
-def performGB(X_train,y_train,X_test,y_test):
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+def performGB(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
     gradient_boosting = GradientBoostingRegressor()
     k_folds = KFold(n_splits = 10)
@@ -79,12 +79,12 @@ def performGB(X_train,y_train,X_test,y_test):
     #db insertion
     # scr = pd.DataFrame({'n_fold': scores.mean(), 'mse': gb_mse, 'rsquare': r2}, index=[0])
     # insertModelMetrics(scr, 'Gradient Boosting')
-    insertPredictedValue(player_predictions, 'Gradient Boosting')
+    # insertPredictedValue(player_predictions, 'Gradient Boosting')
     
     return player_predictions
 
-def performkNN(X_train,y_train,X_test,y_test):
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+def performkNN(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
     knn = KNeighborsRegressor(metric="euclidean")
     prediction_extractor = PredictionExtractor(knn)
@@ -109,11 +109,11 @@ def performkNN(X_train,y_train,X_test,y_test):
     #db insertion
     # scr = pd.DataFrame({'n_fold': scores.mean(), 'mse': mse, 'rsquare': r2}, index=[0])
     # insertModelMetrics(scr, 'K Nearest Neighbors')
-    insertPredictedValue(player_predictions, 'K Nearest Neighbors')
+    # insertPredictedValue(player_predictions, 'K Nearest Neighbors')
     return player_predictions
 
-def performDT(X_train,y_train,X_test,y_test):
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+def performDT(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
     tree = DecisionTreeRegressor( random_state=42, max_depth=5, min_samples_split=2, min_samples_leaf=1)
     prediction_extractor = PredictionExtractor(tree)
@@ -137,11 +137,11 @@ def performDT(X_train,y_train,X_test,y_test):
     #db insertion
     # scr = pd.DataFrame({'n_fold': scores.mean(), 'mse': mse, 'rsquare': r2}, index=[0])
     # insertModelMetrics(scr, 'Decision Tree')
-    insertPredictedValue(player_predictions, 'Decision Tree')
+    # insertPredictedValue(player_predictions, 'Decision Tree')
     return player_predictions
 
-def performLR(X_train,y_train,X_test,y_test):  
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+def performLR(X,y):  
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
     linear_regression = LinearRegression()
     prediction_extractor = PredictionExtractor(linear_regression)
@@ -166,15 +166,15 @@ def performLR(X_train,y_train,X_test,y_test):
     #db insertion
     # scr = pd.DataFrame({'n_fold': scores.mean(), 'mse': mse, 'rsquare': r2}, index=[0])
     # insertModelMetrics(scr, 'Linear Regression')
-    insertPredictedValue(player_predictions, 'Linear Regression')
+    # insertPredictedValue(player_predictions, 'Linear Regression')
 
  
 
     return player_predictions
 
 
-def performRandomForest(X_train,y_train,X_test,y_test):
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+def performRandomForest(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
     random_forest = RandomForestRegressor(n_estimators=100, max_depth=None, min_samples_split=2, min_samples_leaf=1,
                                            min_weight_fraction_leaf=0.0, max_leaf_nodes=None,
                                            min_impurity_decrease=0.0, bootstrap=True,
@@ -202,16 +202,16 @@ def performRandomForest(X_train,y_train,X_test,y_test):
     #db insertion
     # scr = pd.DataFrame({'n_fold': scores.mean(), 'mse': mse, 'rsquare': r2}, index=[0])
     # insertModelMetrics(scr, 'Random Forest')
-    insertPredictedValue(player_predictions, 'Random Forest')
+    # insertPredictedValue(player_predictions, 'Random Forest')
 
     return player_predictions
 
 
 
-def performAdaBoost(X_train,y_train,X_test,y_test):
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+def performAdaBoost(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
-    adaboost = AdaBoostRegressor(n_estimators=50, random_state=42)
+    adaboost = AdaBoostRegressor()
     prediction_extractor = PredictionExtractor(adaboost)
 
     prediction_extractor.fit(X_train, y_train)
@@ -233,7 +233,7 @@ def performAdaBoost(X_train,y_train,X_test,y_test):
     #db insertion
     # scr = pd.DataFrame({'n_fold': scores.mean(), 'mse': mse, 'rsquare': r2}, index=[0])
     # insertModelMetrics(scr, 'AdaBoost')
-    insertPredictedValue(player_predictions, 'AdaBoost')
+    # insertPredictedValue(player_predictions, 'AdaBoost')
     return player_predictions
 
 
@@ -248,28 +248,28 @@ def draw_cross_val(scores,algorithmName):
     plt.grid(True)
     plt.show()
 
-def insertModelMetrics(scores,algorithmName):
-    tz = pytz.timezone('UTC')
-    current_datetime = datetime.datetime.now(tz)
-    connection = create_conn()
-    cursor = connection.cursor()
-    n_fold = scores['n_fold'].values[0]
-    mse = scores['mse'].values[0]
-    rsquare = scores['rsquare'].values[0]
-    insertModelMetricsSql = "INSERT INTO [dbo].[model_metrics] ([model_name], [n_fold_score], [mean_squarred_error], [r_squared], [precision], [recall], [f1_score], [createdAt], [updatedAt]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    cursor.execute(insertModelMetricsSql, (algorithmName, n_fold, mse, rsquare, "", "", "", current_datetime,current_datetime))
-    connection.commit()
-    print("Model metrics inserted successfully")
+# def insertModelMetrics(scores,algorithmName):
+#     tz = pytz.timezone('UTC')
+#     current_datetime = datetime.datetime.now(tz)
+#     connection = create_conn()
+#     cursor = connection.cursor()
+#     n_fold = scores['n_fold'].values[0]
+#     mse = scores['mse'].values[0]
+#     rsquare = scores['rsquare'].values[0]
+#     insertModelMetricsSql = "INSERT INTO [dbo].[model_metrics] ([model_name], [n_fold_score], [mean_squarred_error], [r_squared], [precision], [recall], [f1_score], [createdAt], [updatedAt]) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+#     cursor.execute(insertModelMetricsSql, (algorithmName, n_fold, mse, rsquare, "", "", "", current_datetime,current_datetime))
+#     connection.commit()
+#     print("Model metrics inserted successfully")
 
-def insertPredictedValue(player_predictions,algorithmName):
-    tz = pytz.timezone('UTC')
-    current_datetime = datetime.datetime.now(tz)
-    connection = create_conn()
-    cursor = connection.cursor()
-    insertPredictedValueSql = "INSERT INTO [dbo].[prediction_results] ([name], [market_value_in_eur], [model_prediction],[prediction_model],[createdAt], [updatedAt]) VALUES (?, ?, ?, ?, ?, ?)"
-    data = [(row['player'],row['market_value_in_eur'] ,row['model_prediction'],algorithmName,current_datetime, current_datetime) for index, row in player_predictions.iterrows()]
-    cursor.executemany(insertPredictedValueSql, data)
-    connection.commit()
+# def insertPredictedValue(player_predictions,algorithmName):
+#     tz = pytz.timezone('UTC')
+#     current_datetime = datetime.datetime.now(tz)
+#     connection = create_conn()
+#     cursor = connection.cursor()
+#     insertPredictedValueSql = "INSERT INTO [dbo].[prediction_results] ([name], [market_value_in_eur], [model_prediction],[prediction_model],[createdAt], [updatedAt]) VALUES (?, ?, ?, ?, ?, ?)"
+#     data = [(row['player'],row['market_value_in_eur'] ,row['model_prediction'],algorithmName,current_datetime, current_datetime) for index, row in player_predictions.iterrows()]
+#     cursor.executemany(insertPredictedValueSql, data)
+#     connection.commit()
 class PredictionExtractor(BaseEstimator, TransformerMixin):
     def __init__(self, model):
         self.model = model
@@ -288,44 +288,44 @@ class PredictionExtractor(BaseEstimator, TransformerMixin):
 
     
 
-# print("Output of Linear Regression model:")
-# print(performLR(X,y))
+print("Output of Linear Regression model:")
+print(performLR(X,y))
 
-# print("Output of Gradient Boosting model:")
-# print(performGB(X,y))
+print("Output of Gradient Boosting model:")
+print(performGB(X,y))
 
-# print("Output of K-Nearest Neighbors model:")
-# print(performkNN(X,y))
+print("Output of K-Nearest Neighbors model:")
+print(performkNN(X,y))
 
-# print("Output of Decision Tree model:")
-# print(performDT(X,y))
-
-
-# print("Output of Random Forest model:")
-# print(performRandomForest(X, y))
+print("Output of Decision Tree model:")
+print(performDT(X,y))
 
 
-# print("Output of AdaBoost model:")
-# print(performAdaBoost(X, y))
+print("Output of Random Forest model:")
+print(performRandomForest(X, y))
+
+
+print("Output of AdaBoost model:")
+print(performAdaBoost(X, y))
     
-from sklearn.model_selection import KFold
+# from sklearn.model_selection import KFold
 
-# Define the number of folds
-k_folds = KFold(n_splits=10)
+# # Define the number of folds
+# k_folds = KFold(n_splits=10)
 
-# Define your algorithms
-algorithms = [performkNN, performDT, performLR, performRandomForest, performAdaBoost, performGB]
+# # Define your algorithms
+# algorithms = [performkNN, performDT, performLR, performRandomForest, performAdaBoost, performGB]
 
-# Split the data into k folds
-for train_index, test_index in k_folds.split(X):
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+# # Split the data into k folds
+# for train_index, test_index in k_folds.split(X):
+#     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+#     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
     
-    # For each algorithm
-    for algorithm in algorithms:
-        X_t = X_train.copy()
-        y_t = y_train.copy()
-        X_te = X_test.copy()
-        y_te = y_test.copy()
-        # Perform cross-validation
-        algorithm(X_t, y_t, X_te, y_te)
+#     # For each algorithm
+#     for algorithm in algorithms:
+#         X_t = X_train.copy()
+#         y_t = y_train.copy()
+#         X_te = X_test.copy()
+#         y_te = y_test.copy()
+#         # Perform cross-validation
+#         algorithm(X_t, y_t, X_te, y_te)
